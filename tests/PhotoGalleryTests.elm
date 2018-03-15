a5610b8d5004b6ead00a8b81df7583270ded2716
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-03-15 19:08:56 -0400

    Test that thumbnails are rendered correctly.

--- tests/PhotoGalleryTests.elm
+++ tests/PhotoGalleryTests.elm
@@ -2,7 +2,10 @@ module PhotoGalleryTests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Html.Attributes as Attr
 import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (text, tag, attribute)
 import PhotoGallery exposing (..)
 import Json.Decode exposing (decodeValue)
 import Json.Encode as Encode
@@ -43,6 +46,44 @@ stateTransitions =
                         |> .selectedUrl
                         |> Expect.equal (List.head urls)
         ]
+
+
+doesNotRenderThumbnailsWhenListIsEmpty : Test
+doesNotRenderThumbnailsWhenListIsEmpty =
+    test "No thumbnails render when there are no photos to render." <|
+        \_ ->
+            PhotoGallery.model
+                |> PhotoGallery.view
+                |> Query.fromHtml
+                |> Query.findAll [ tag "img" ]
+                |> Query.count (Expect.equal 0)
+
+
+rendersEachThumbnailInTheList : Test
+rendersEachThumbnailInTheList =
+    fuzz (Fuzz.intRange 1 5) "URLs render as thumbnails" <|
+        \urlCount ->
+            let
+                thumbnailChecks : List (Query.Single msg -> Expectation)
+                thumbnailChecks =
+                    List.map thumbnailRendered urls
+
+                urls : List String
+                urls =
+                    List.range 1 urlCount
+                        |> List.map (\num -> toString num ++ ".png")
+            in
+                { model | photos = List.map photoFromUrl urls }
+                    |> PhotoGallery.view
+                    |> Query.fromHtml
+                    |> Expect.all thumbnailChecks
+
+
+thumbnailRendered : String -> Query.Single msg -> Expectation
+thumbnailRendered url query =
+    query
+        |> Query.findAll [ tag "img", attribute <| Attr.src (urlPrefix ++ url) ]
+        |> Query.count (Expect.atLeast 1)
 
 
 photoFromUrl : String -> Photo
